8e34f13b00eb0dd14bf109856685fddf26b210ff
Ben Hackett <enjaminb@yahoo.com>
2012-03-18 20:44:04 -0700

    commintg again

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , mongoose = require('mongoose').mongoose
+  , db = mongoose.connect('mongodb://localhost/nodepad')
+  , Document = require('./models.js').Document(db);
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.logger());
+  app.use(express.errorHandler({
+  	dumpExceptions:true,
+  	showStack:true
+  }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+  app.use(express.logger());
+});
+
+app.configure(function(){
+	app.use(express.logger());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
